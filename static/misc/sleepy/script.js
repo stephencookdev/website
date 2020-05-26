a67e5852e8579e9c0ef8488e8c2fd7d6dc7eacbd
Stephen Cook <stephen@stephencookdev.co.uk>
2020-05-26 23:54:17 +0100

    Add the basics of a sleepy app

---
+++ static/misc/sleepy/script.js
@@ -0,0 +1,71 @@
+const generateSoundToVolumeMap = (soundButtons) => {
+  const map = {};
+  soundButtons.forEach((button) => {
+    const audio = new Audio(`sounds/${button.dataset.sound}`);
+    map[button.dataset.sound] = audio;
+    audio.volume = 0;
+    audio.loop = true;
+  });
+
+  return map;
+};
+
+const triggerSoundChange = (soundKey, val, soundToVolumeMap) => {
+  const audio = soundToVolumeMap[soundKey];
+  audio.volume = val / 100;
+
+  if (audio.paused && val > 0) {
+    audio.play();
+  } else if (!audio.paused && val === 0) {
+    audio.pause();
+  }
+};
+
+const getButtonRange = (button) => button.querySelector('input[type="range"]');
+
+const addSoundButtonListeners = (soundButtons, soundToVolumeMap) => {
+  soundButtons.forEach((button) => {
+    const buttonRange = getButtonRange(button);
+
+    button.addEventListener("click", () => {
+      if (buttonRange.value > 0) buttonRange.value = 0;
+      else buttonRange.value = 100;
+
+      triggerSoundChange(
+        button.dataset.sound,
+        buttonRange.value,
+        soundToVolumeMap
+      );
+    });
+
+    buttonRange.addEventListener("click", (e) => {
+      e.stopPropagation();
+    });
+    buttonRange.addEventListener("change", () => {
+      triggerSoundChange(
+        button.dataset.sound,
+        buttonRange.value,
+        soundToVolumeMap
+      );
+    });
+  });
+};
+
+const initialiseMixer = (soundButtons, soundToVolumeMap) => {
+  soundButtons.forEach((button) => {
+    const buttonRange = getButtonRange(button);
+    buttonRange.value = soundToVolumeMap[button.dataset.sound].volume * 100;
+
+    triggerSoundChange(
+      button.dataset.sound,
+      buttonRange.value,
+      soundToVolumeMap
+    );
+  });
+};
+
+const soundButtons = document.querySelectorAll(".sound");
+const soundToVolumeMap = generateSoundToVolumeMap(soundButtons);
+
+addSoundButtonListeners(soundButtons, soundToVolumeMap);
+initialiseMixer(soundButtons, soundToVolumeMap);
