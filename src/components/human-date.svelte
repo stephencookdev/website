d398a3f8da36845e7798be43ae8de92baad160a6
Stephen Cook <stephen@stephencookdev.co.uk>
2020-06-12 11:40:07 +0100

    SSR the date component correctly

--- src/components/human-date.svelte
+++ src/components/human-date.svelte
@@ -1,9 +1,25 @@
 <script>
   export let date;
 
-  const dateHuman = new Intl.DateTimeFormat("en", {
-    dateStyle: "medium"
-  }).format(new Date(date));
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  $: dateObj = new Date(date);
+  $: dateHuman = `${dateObj.getDate()} ${
+    monthNames[dateObj.getMonth()]
+  } ${dateObj.getFullYear()}`;
 </script>
 
 <time datetime={date}>&nbsp;{dateHuman}&nbsp;</time>
